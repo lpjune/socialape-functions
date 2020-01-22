3163d1c56d0e380393d24338622d86eb1e92c501
lpjune <laurenporsini@gmail.com>
2020-01-22 15:14:37 -0600

    #13 onimagechange, onscreamdelete

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -65,7 +65,11 @@ exports.signup = (req, res) => {
                     .status(400)
                     .json({ email: "Email is already in use" });
             } else {
-                return res.status(500).json({ error: err.code });
+                return res
+                    .status(500)
+                    .json({
+                        general: "Something went wrong, please try again"
+                    });
             }
         });
 };
@@ -92,11 +96,11 @@ exports.login = (req, res) => {
         })
         .catch(err => {
             console.error(err);
-            if (err.code === "auth/wrong-password") {
-                return res
-                    .status(403)
-                    .json({ general: "Wrong credentials, please try again" });
-            } else return res.status(500).json({ error: err.code });
+            // auth/wrong-password
+            // auth/user-not-found
+            return res
+                .status(403)
+                .json({ general: "Wrong credentials, please try again" });
         });
 };
 
